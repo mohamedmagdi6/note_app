b631ca1b27df02b523ff22cfc54aad1fea1fe23b
mohamedmagdi6 <mohamedmagdii137@gmail.com>
2024-02-09 14:53:44 +0200

    create custom app bar

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/custom_app_bar.dart';
+
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold(
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 40,
+            ),
+            CustomAppBar()
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/widgets/custom_app_bar.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'custom_search_icon.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          'Notes',
+          style: TextStyle(
+            fontSize: 32,
+          ),
+        ),
+        CustomSearchIcon(),
+      ],
+    );
+  }
+}
