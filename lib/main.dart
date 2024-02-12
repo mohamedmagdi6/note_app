378bbd020d85dedcf3ef6125d997063b3adece5f
mohamedmagdi6 <mohamedmagdii137@gmail.com>
2024-02-12 17:14:36 +0200

    add simple bloc opserver

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app/constants.dart';
 import 'package:notes_app/cubits/add_notes_cubit/add_notes_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
+import 'package:notes_app/models/simple_bloc_observer.dart';
 import 'package:notes_app/views/notes_view.dart';
 
 void main() async {
@@ -11,6 +12,7 @@ void main() async {
   await Hive.openBox(kNotesBox);
   Hive.registerAdapter(
       NoteModelAdapter()); // to make hive considered to NoteModel. => register adaptor process
+  Bloc.observer = SimpleBlocOberver();
   runApp(const NotesApp());
 }
 
